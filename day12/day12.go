3a533002b6ce2373b7758d76f8eaa49b040b945d
Matteo Vaccari <vaccari@pobox.com>
2024-12-20 21:12:24 +0100

    Day12: merging ids

--- day12/day12.go
+++ day12/day12.go
@@ -9,8 +9,8 @@ import (
 type Plant int32
 
 type RegionSet struct {
-	regions map[RegionId][]mapping.Coord
-	plot    mapping.Map
+	ids  [][]RegionId
+	plot mapping.Map
 }
 
 type RegionId int
@@ -24,29 +24,32 @@ type ReportLine struct {
 
 func NewRegionSet(plot mapping.Map) RegionSet {
 	return RegionSet{
-		regions: make(map[RegionId][]mapping.Coord),
-		plot:    plot,
+		ids:  initRegionIds(plot),
+		plot: plot,
 	}
 }
 
+func initRegionIds(plot mapping.Map) [][]RegionId {
+	ids := matrix.New[RegionId](plot.Rows(), plot.Cols())
+	i := 0
+	plot.ForEach(func(r int, c int, value int32) {
+		i++
+		ids[r][c] = RegionId(i)
+	})
+	return ids
+}
+
 func (rs RegionSet) Report() Report {
-	ids := rs.initRegionIds()
+	rs.mergeRegionIds()
 	areas := make(map[RegionId]int)
 	perims := make(map[RegionId]int)
 	plants := make(map[RegionId]Plant)
 	rs.plot.ForEachCoord(func(c mapping.Coord, value int32) {
-		id := ids[c.Row][c.Col]
+		id := rs.ids[c.Row][c.Col]
 		plant := Plant(value)
 		plants[id] = plant
-
 		areas[id] = areas[id] + 1
-
-		var perimeter int
-		for _, coord := range c.OrthoNeighbors() {
-			if !rs.plot.IsValid(coord) || ids[coord.Row][coord.Col] != id {
-				perimeter++
-			}
-		}
+		perimeter := rs.perimeter(c, id)
 		perims[id] = perims[id] + perimeter
 	})
 	var result []ReportLine
@@ -60,14 +63,40 @@ func (rs RegionSet) Report() Report {
 	return result
 }
 
-func (rs RegionSet) initRegionIds() [][]RegionId {
-	ids := matrix.New[RegionId](rs.plot.Rows(), rs.plot.Cols())
-	i := 0
-	rs.plot.ForEach(func(r int, c int, value int32) {
-		i++
-		ids[r][c] = RegionId(i)
+func (rs RegionSet) perimeter(c mapping.Coord, id RegionId) int {
+	var perimeter int
+	for _, coord := range c.OrthoNeighbors() {
+		if !rs.plot.IsValid(coord) || rs.ids[coord.Row][coord.Col] != id {
+			perimeter++
+		}
+	}
+	return perimeter
+}
+
+func (rs RegionSet) mergeRegionIds() {
+	rs.plot.ForEachCoord(func(c mapping.Coord, value int32) {
+		plant := Plant(value)
+		id := rs.regionIdOf(c)
+		for _, neighbor := range c.OrthoNeighbors() {
+			if rs.plot.IsValid(neighbor) {
+				neighborPlant := Plant(rs.plot.At(neighbor))
+				neighborId := rs.regionIdOf(neighbor)
+				if plant == neighborPlant && id != neighborId {
+					l := min(id, neighborId)
+					rs.setRegionId(c, l)
+					rs.setRegionId(neighbor, l)
+				}
+			}
+		}
 	})
-	return ids
+}
+
+func (rs RegionSet) regionIdOf(c mapping.Coord) RegionId {
+	return rs.ids[c.Row][c.Col]
+}
+
+func (rs RegionSet) setRegionId(c mapping.Coord, id RegionId) {
+	rs.ids[c.Row][c.Col] = id
 }
 
 func (r Report) Strings() []string {
