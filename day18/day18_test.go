80be58325bbf188b6553aa21e38c3fec9395aa6e
Matteo Vaccari <vaccari@pobox.com>
2025-11-17 22:34:35 +0100

    day18 part2 solved

--- day18/day18_test.go
+++ day18/day18_test.go
@@ -80,6 +80,12 @@ func TestPart2(t *testing.T) {
 			input:    parseInput1("5,4\n4,2\n4,5\n3,0\n2,1\n6,3\n2,4\n1,5\n0,6\n3,3\n2,6\n5,1\n1,2\n5,5\n2,5\n6,5\n1,4\n0,4\n6,4\n1,1\n6,1\n1,0\n0,5\n1,6\n2,0"),
 			expected: "6,1",
 		},
+		{
+			name:     "full problem",
+			size:     71,
+			input:    parseInput1(readFile("input.txt")),
+			expected: "60,21",
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
